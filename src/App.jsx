03221276146ca7af254c3c8f5d88d8c044a9e761
LAPTOP-15B32R05\Umar <umarinayat111@gmail.com>
2024-05-21 01:22:44 +0500

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MultiStepForm from './MultiStepForm';
 import MultiStepFormExtended from './MultiStepFormExtended';
-import Docu from './Docu';
-import DocuSignComponent from './DocuSignComponent';
-import ServerTest from './ServerTest';
 
 const App = () => {
     return (
@@ -11,9 +8,6 @@ const App = () => {
             <Routes>
                 <Route path="/" element={<MultiStepForm />} />
                 <Route path="/extended-form" element={<MultiStepFormExtended />} />
-                {/* <Route path='/doc' element={<Docu/>}/> */}
-                {/* <Route path='/doc' element={<DocuSignComponent/>} /> */}
-                <Route path='/server' element={<ServerTest/>}/>
             </Routes>
         </Router>
     );
